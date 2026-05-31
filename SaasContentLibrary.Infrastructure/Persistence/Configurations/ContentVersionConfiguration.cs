553029410de4e26fe0b6a00f0459c5044685b8aa
raygunathilake <patience0112@gmail.com>
2026-05-31 22:36:48 +0530

    Added version configurations

--- SaasContentLibrary.Infrastructure/Persistence/Configurations/ContentVersionConfiguration.cs
+++ SaasContentLibrary.Infrastructure/Persistence/Configurations/ContentVersionConfiguration.cs
@@ -1,10 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SaasContentLibrary.Domain.ContentBlocks;
 
-namespace SaasContentLibrary.Infrastructure.Persistence.Configurations
+namespace SaasContentLibrary.Infrastructure.Persistence.Configurations;
+
+public sealed class ContentVersionConfiguration : IEntityTypeConfiguration<ContentVersion>
 {
-    internal class ContentVersionConfiguration
+    public void Configure(EntityTypeBuilder<ContentVersion> builder)
     {
+        builder.ToTable("content_versions");
+
+        builder.HasKey(v => v.Id);
+        builder.Property(v => v.Id)
+            .HasConversion(id => id.Value, value => new ContentVersionId(value))
+            .ValueGeneratedNever();
+
+        builder.Property(v => v.VersionNumber);
+
+        builder.Property(v => v.ContentBody)
+            .HasConversion(
+               contentBody => contentBody.Value,
+                value => ContentBody.Create(value).Value)
+            .IsRequired()
+            .HasMaxLength(ContentBody.MaxLength);
+
+        builder.Property(v => v.VersionStatus)
+            .HasConversion<string>()
+            .HasMaxLength(20);
+
+        builder.Property(v => v.AuthoredBy)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(v => v.AuthoredOnUtc);
+        builder.Property(v => v.ApprovedBy)
+            .HasMaxLength(200);
+        builder.Property(v => v.ApprovedOnUtc);
+
+        builder.HasIndex("ContentBlockId", nameof(ContentVersion.VersionNumber))
+            .IsUnique();
     }
 }
