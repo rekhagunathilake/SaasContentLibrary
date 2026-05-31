b2c3034b900a853453abb089c7e9d2fb83cc7ff6
raygunathilake <patience0112@gmail.com>
2026-05-31 22:30:01 +0530

    Added content block configurations and db context class

--- SaasContentLibrary.Infrastructure/Persistence/SaasContentLibraryDbContext.cs
+++ SaasContentLibrary.Infrastructure/Persistence/SaasContentLibraryDbContext.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using SaasContentLibrary.Domain.ContentBlocks;
 
-namespace SaasContentLibrary.Infrastructure.Persistence
+namespace SaasContentLibrary.Infrastructure.Persistence;
+
+public sealed class SaasContentLibraryDbContext(DbContextOptions<SaasContentLibraryDbContext> options) : DbContext(options)
 {
-    internal class SaasContentLibraryDbContext
+    public DbSet<ContentBlock> ContentBlocks => Set<ContentBlock>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.HasDefaultSchema("content");
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(SaasContentLibraryDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
     }
 }
