5041a744fc8276f9d9f0ff15a8b14f516577bef6
raygunathilake <patience0112@gmail.com>
2026-05-31 22:05:31 +0530

    Added layout for infrastructure layer

---
+++ SaasContentLibrary.Infrastructure/Persistence/Configurations/ContentBlockConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SaasContentLibrary.Infrastructure.Persistence.Configurations
+{
+    internal class ContentBlockConfiguration
+    {
+    }
+}
---
+++ SaasContentLibrary.Infrastructure/Persistence/Repositories/ContentBlockRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SaasContentLibrary.Infrastructure.Persistence.Repositories
+{
+    internal class ContentBlockRepository
+    {
+    }
+}
